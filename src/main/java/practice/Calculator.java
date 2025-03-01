16c8b6382e9319f43ac575240c47928892ea22c0
SuchitraKhandale <suchitrakhandale5@gmail.com>
2025-03-01 22:30:30 +0530

    practice commite one addition

---
+++ src/main/java/practice/Calculator.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Addition---");
+        System.out.println( 5+5 );
+    }
+}
