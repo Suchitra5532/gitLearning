50eb88995760a00f23e776b5a4405c74afdcaddc
SuchitraKhandale <suchitrakhandale5@gmail.com>
2025-03-01 22:38:16 +0530

    practice 3 Substraction

--- src/main/java/practice/Calculator.java
+++ src/main/java/practice/Calculator.java
@@ -11,5 +11,9 @@ public class Calculator {
         System.out.println("Now Sub");
 
         System.out.println("Helloooooooooooooooooooogit");
+
+        System.out.println("Subbbb");
+
+        System.out.println(9-6);
     }
 }
