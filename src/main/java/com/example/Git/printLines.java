7509b6e3f8225355236ef94af70e25751de9e200
SuchitraKhandale <suchitrakhandale5@gmail.com>
2025-03-01 22:13:42 +0530

    revertCode

--- src/main/java/com/example/Git/printLines.java
+++ src/main/java/com/example/Git/printLines.java
@@ -5,9 +5,5 @@ public class printLines {
     public static void main(String[] args) {
 
         System.out.println("printLine1"+1);
-
-        System.out.println("PrintLine2"+2);
-
-        System.out.println("PrintLine3"+3);
     }
 }
